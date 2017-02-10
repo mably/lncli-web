18f3a829dfd35ae50a043fa9a79beaa7d3ca2f46
Mably <f.mably@gmail.com>
2017-02-11 00:41:01 +0100

    socket.io experimentation for streaming + various fixes

---
+++ app/sockets.js
@@ -0,0 +1,59 @@
+// app/sockets.js
+
+// TODO
+module.exports = function(io, lightning) {
+
+	var clients = [];
+
+	var subscribeInvoicesCall = null;
+
+	var initSubscribeInvoicesCall = function() {
+
+		if (!subscribeInvoicesCall) {
+
+			console.log("Registering to lnd SubscribeInvoices stream");
+
+			subscribeInvoicesCall = lightning.subscribeInvoices({});
+
+			subscribeInvoicesCall.on("data", function(data) {
+				console.log("SubscribeInvoices Data", data);
+				for (var i = 0; i < clients.length; i++) {
+					clients[i].emit("invoice", data);
+				}
+			});
+
+			subscribeInvoicesCall.on("end", function() {
+				console.log("SubscribeInvoices End");
+			});
+
+			subscribeInvoicesCall.on("error", function(err) {
+				console.log("SubscribeInvoices Error", err);
+			});
+
+			subscribeInvoicesCall.on("status", function(status) {
+				console.log("SubscribeInvoices Status", status);
+			});
+		}
+	}
+
+	io.on("connection", function(socket) {
+
+		/** printing out the client who joined */
+		console.log("New client connected (id=" + socket.id + ").");
+		
+		socket.emit("hello", { message: "hello world!" });
+
+		/** pushing new client to client array*/
+		clients.push(socket);
+
+		initSubscribeInvoicesCall();
+		
+		/** listening if client has disconnected */
+		socket.on("disconnect", function() {
+			clients.splice(clients.indexOf(socket), 1);
+			console.log("client disconnected (id=" + socket.id + ").");
+		});
+
+	});
+
+}
