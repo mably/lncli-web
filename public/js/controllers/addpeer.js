90a8874e019e5793839a3ecd3cbdd94819e5be34
Mably <f.mably@gmail.com>
2017-02-11 21:34:30 +0100

    Fixing missing injected $scope

--- public/js/controllers/addpeer.js
+++ public/js/controllers/addpeer.js
@@ -1,8 +1,8 @@
 (function () {
 
-	lnwebcli.controller("ModalAddPeerCtrl", ["$uibModalInstance", "defaults", "lncli", controller]);
+	lnwebcli.controller("ModalAddPeerCtrl", ["$scope", "$uibModalInstance", "defaults", "lncli", controller]);
 
-	function controller ($uibModalInstance, defaults, lncli) {
+	function controller ($scope, $uibModalInstance, defaults, lncli) {
 
 		var $ctrl = this;
 
