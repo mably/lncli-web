f31b03d88e20fedfc2fcc9e6f0f23f073bccf021
f.masurel <fmasurel@bordeaux-metropole.fr>
2017-03-21 16:49:20 +0100

    Two-phase commit implemented for tipping transactions

--- app/database.js
+++ app/database.js
@@ -1,5 +1,5 @@
 // app/database.js
-var Db = require('tingodb')().Db;
+var Db = require('tingodb')({ searchInArray: true }).Db;
 
 // expose the routes to our app with module.exports
 module.exports = function(dataPath) {
