e3333230c7aa35cd4b258ac2a5710e847ab50067
Mably <f.mably@gmail.com>
2017-03-26 09:53:48 +0200

    added gulp task to duplicate example configuration files on npm install if missing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const jscs = require("gulp-jscs");
 const jshint = require("gulp-jshint");
+const fs = require("fs");
+const gutil = require("gulp-util");
 
 gulp.task("jscs", () => {
 	return gulp.src(["app/**/*.js","config/**/*.js","public/js/**/*.js"], { base: "." })
@@ -26,4 +28,25 @@ gulp.task("lint", function() {
 		}));
 });
 
-gulp.task("default", ["jscs", "lint"]);
+gulp.task('js-dist', function() {
+	fs.readdirSync('config')
+		.filter(function(file) {
+			return file.substr(-11) === '-example.js';
+		})
+		.forEach(function(file) {
+			var filename = file.substr(0, file.length - 11);
+			try {
+				fs.statSync('config/' + filename + '.js');
+			} catch(err) { // file doesn't exist
+				gutil.log('Initializing', gutil.colors.cyan(filename + '.js'), 'file from example...');
+				fs.writeFileSync('config/' + filename + '.js',
+					fs.readFileSync('config/' + filename + '-example.js'));
+			}
+		});
+});
+
+gulp.task("install", ["js-dist"]);
+
+gulp.task("check", ["jscs", "lint"]);
+
+gulp.task("default", ["install", "check"]);
