5d16fe2db258db62196b24ee6cbfea0db4eef2ef
mably <f.mably@gmail.com>
2017-07-13 13:01:00 +0200

    Allows hosting the HTML client on a different host than the server (Chrome Extension)

--- public/js/factories/utils.js
+++ public/js/factories/utils.js
@@ -13,6 +13,25 @@
 				var uint8Array = new Uint8Array(buffer);
 				if (reverse) { uint8Array = uint8Array.reverse(); }
 				return Array.prototype.map.call(uint8Array, x => ("00" + x.toString(16)).slice(-2)).join("");
+			},
+
+			/**
+			 * TODO
+			 */
+			getUrlParameterByName: function (name, url) {
+				if (!url) {
+					url = window.location.href;
+				}
+				name = name.replace(/[\[\]]/g, "\\$&");
+				var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+				var results = regex.exec(url);
+				if (!results) {
+					return null;
+				}
+				if (!results[2]) {
+					return "";
+				}
+				return decodeURIComponent(results[2].replace(/\+/g, " "));
 			}
 
 		};
