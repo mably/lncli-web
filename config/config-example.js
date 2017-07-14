31c8eba485d4b0452394b71c61cdb1457f3e58d6
Mably <f.mably@gmail.com>
2017-07-14 22:34:53 +0200

    New default payreq for authentication

--- config/config-example.js
+++ config/config-example.js
@@ -3,5 +3,5 @@
 module.exports = {
 	sessionSecret: "my session secret",
 	sessionMaxAge: 300000,
-	defaultAuthPayReq: "yxd4apurxhrqwuum1tpyqyyju8d8zx6z63ncan5yugw94cenn8an5y51yddg5wh3jxyomepq7sfpj868s6ekty47scuhpbw5qg1aahyiyyyyyyyyyyyyd3pogfgo"
+	defaultAuthPayReq: "ymdeeamm664q4wpo56hrdiq5fhf3ai1zfz768rhwf9effzsxty9z6apseqzqfoibbb7qgh1fedt3yt6oa3dd31eucedkob4azyzxwy8syyyyyyyyyyyyne6xgrjy"
 };
