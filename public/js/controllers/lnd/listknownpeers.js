00b436acbf0ead90bcc60e452cd1c80edb8e1bab
Mably <f.mably@gmail.com>
2017-07-14 22:55:07 +0200

    Missing bootbox import fix

--- public/js/controllers/lnd/listknownpeers.js
+++ public/js/controllers/lnd/listknownpeers.js
@@ -1,7 +1,7 @@
 (function () {
 	"use strict";
 
-	module.exports = function ($rootScope, $scope, $timeout, $uibModal, $, lncli, config) {
+	module.exports = function ($rootScope, $scope, $timeout, $uibModal, $, bootbox, lncli, config) {
 
 		var $ctrl = this;
 
@@ -69,14 +69,16 @@
 		};
 
 		$scope.remove = function (peer) {
-			bootbox.confirm("Do you really want to remove that peer?", function () {
-				lncli.removeKnownPeer(peer.pub_key).then(function (response) {
-					console.log("RemoveKnownPeer removed=", response);
-					$scope.refresh();
-				}, function (err) {
-					console.log(err);
-					lncli.alert(err.message);
-				});
+			bootbox.confirm("Do you really want to remove that peer?", function (result) {
+				if (result) {
+					lncli.removeKnownPeer(peer.pub_key).then(function (response) {
+						console.log("RemoveKnownPeer removed=", response);
+						$scope.refresh();
+					}, function (err) {
+						console.log(err);
+						lncli.alert(err.message);
+					});
+				}
 			});
 		};
 
