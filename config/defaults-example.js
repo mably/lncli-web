24b2bc2b1d7cb1da34e217c72830117738794fa2
Haafingar <carnage104@gmail.com>
2017-09-06 14:30:19 +1000

    Change Cert default filepath to match Readme
    Resoluton to issue 2

--- config/defaults-example.js
+++ config/defaults-example.js
@@ -5,7 +5,7 @@ module.exports = {
 	serverHost: "localhost",
 	lndProto: __dirname + "/rpc.proto",
 	lndHost: "localhost:10009",
-	lndCertPath: __dirname + "/../lnd.cert",
+	lndCertPath: __dirname + "/tls.cert",
 	dataPath: __dirname + "/../data",
 	loglevel: "info",
 	logfile: "lncliweb.log",
